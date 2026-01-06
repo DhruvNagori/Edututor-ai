78959917793383f9e70fdc15d2f18c344c058cd7
DhruvNagori <168324346+DhruvNagori@users.noreply.github.com>
2026-01-06 18:26:52 +0530

    Updated function in Memory file

--- app/memory.py
+++ app/memory.py
@@ -1,19 +1,3 @@
-# from langchain_core.messages import HumanMessage, AIMessage
-
-# class ChatMemory:
-#     def __init__(self):
-#         self.history = []
-
-#     def add_user(self, text: str):
-#         self.history.append(HumanMessage(content=text))
-
-#     def add_ai(self, text: str):
-#         self.history.append(AIMessage(content=text))
-
-#     def get(self):
-#         return self.history
-
-
 from langchain_core.messages import HumanMessage, AIMessage
 
 class ChatMemory:
